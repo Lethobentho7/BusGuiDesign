43edad55d53f4f0be964a3c6422866556d0befaa
vodka <vodka_628@163.com>
2022-10-14 15:04:19 +0800

    BusGuiTest

---
+++ test/RunRoadMapTest.py
@@ -0,0 +1,17 @@
+import sys
+import RoadMap
+# 导入相关模块
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = QMainWindow()
+    # 控件由xx自动生成的类接管
+    # 创建一个类实例
+    ui = RoadMap.Ui_MainWindow()
+    # 向主窗口上添加控件
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    # 进入主循环
+    sys.exit(app.exec_())
